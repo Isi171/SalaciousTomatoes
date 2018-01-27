6ef655baec4cdaf5cafad6199e3d9fe3e3eff723
Federixender <federixender@hotmail.it>
2018-01-27 09:58:15 +0100

    Gene data structure

---
+++ Assets/Scripts/Gene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gene : MonoBehaviour {
+	public enum RPS {Rock, Paper, Scissors, Zero};
+	public enum Strength {Small, Medium, Big};
+	RPS rpsValue;
+	Strength strValue;
+
+	public RPS RpsValue {
+		get{return rpsValue; }
+		set{ rpsValue = value; }
+	}
+
+	public Strength StrValue {
+		get{return strValue;}
+		set{ strValue = value; }
+	}
+}
