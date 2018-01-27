fe1c4eea493a79f559d979e8020d16952ea082cc
Isi171 <filippo.agalbato@cleafy.com>
2018-01-27 11:35:42 +0100

    Objective card prefab

---
+++ Assets/Scripts/ObjectiveHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveHandler : MonoBehaviour
+{
+	[SerializeField] private Text geneRequestText;
+	[SerializeField] private Text timerText;
+	[SerializeField] private Image icon;
+}
