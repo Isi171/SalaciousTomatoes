42fe217fc51e25a5c6e9fb2f9dd47c2d5a25423a
Marco Ballabio <marco.ballabio@outlook.it>
2018-01-27 10:09:55 +0100

    Imported character animator and created some materials.

---
+++ Assets/Scripts/Tests/MaterialTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialTest : MonoBehaviour {
+
+    [SerializeField] private SkinnedMeshRenderer smr;
+    [SerializeField] Material m;
+
+	// Use this for initialization
+	void Start () {
+        smr.material = m;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
