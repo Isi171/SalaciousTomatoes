aef401611a2fbcf8c6bfb731f2314da4d3c2eb5f
Isi171 <filippo.agalbato@cleafy.com>
2018-01-27 20:03:36 +0100

    GUESS WHAT MOTHERFU-- menus

---
+++ Assets/Scripts/Menus/InGameContextualMenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameContextualMenuController : MonoBehaviour
+{
+    public InputHandler inputHandler;
+    public RectTransform panel;
+    
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        panel.localScale = new Vector3(1, 1, 1);
+        inputHandler.stopInput = true;
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1;
+        panel.localScale = new Vector3(0, 0, 1);
+        inputHandler.stopInput = false;
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Music()
+    {
+        
+    }
+
+    public void Sound()
+    {
+        
+    }
+}
