69369781247eb200cc7f40c47ccb464256d3f0fe
Federixender <federixender@hotmail.it>
2018-01-27 11:15:22 +0100

    the Gene class is the data structure for genes and has a method to compare two genes strengthwise

--- Assets/Scripts/Gene.cs
+++ Assets/Scripts/Gene.cs
@@ -8,6 +8,44 @@ public class Gene : MonoBehaviour {
 	RPS rpsValue;
 	Strength strValue;
 
+	/**
+	 * returns -1 if the current gene is stronger than the new one;
+	 * returns 0 if both genes are of equal strength;
+	 * returns 1 if the new gene is stronger than the old one.
+	 */
+	public static int Equals (Gene currentGene, Gene newGene){
+		if (newGene.RpsValue == Gene.RPS.Zero) {
+			return -1; //the new gene is neutral and is weaker than any other.
+		} else if (currentGene.RpsValue == Gene.RPS.Zero) {
+			return 1; //the old gene is neutral and is weaker than any other
+		} else if (currentGene.RpsValue == newGene.RpsValue) { //both gene types are the same; their strengths are compared.
+			if (currentGene.StrValue == newGene.StrValue) {
+				return 0; //both genes have the same values and are of equal strength.
+			} else if (newGene.StrValue == Gene.Strength.Big) {
+				return 1; //the new gene is big and the old gene is small or medium; the new gene is stronger.
+			} else if (newGene.StrValue == Gene.Strength.Medium) {
+				if (currentGene.StrValue == Gene.Strength.Small) {
+					return 1; //the new gene is medium and the old gene is small; the new gene is stronger
+				} else {
+					return -1; //the new gene is medium and the old gene is big; the old gene is stronger.
+				}
+			} else { //the new gene is small
+				return -1; //the new gene is small and the old gene is medium or big; the old gene is stronger.
+			}
+		} else { //gene types are different; the strengths are ignored and the types are compred
+			if (newGene.RpsValue == Gene.RPS.Rock && currentGene.RpsValue == Gene.RPS.Scissors) {
+				return 1; //rock beats scissors; the new gene is stronger.
+			} else if (newGene.RpsValue == Gene.RPS.Scissors && currentGene.RpsValue == Gene.RPS.Paper) {
+				return 1; //scissors beat paper; the new gene is stronger.
+			} else if (newGene.RpsValue == Gene.RPS.Paper && currentGene.RpsValue == Gene.RPS.Rock) {
+				return 1; //paper beats rock; the new gene is stronger.
+			} else {
+				return -1; //the old gene is stronger.
+			}
+		}
+
+	}
+
 	public RPS RpsValue {
 		get{return rpsValue; }
 		set{ rpsValue = value; }
