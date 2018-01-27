67544472148522dbf25d9ec802515aac734cec7a
Federixender <federixender@hotmail.it>
2018-01-27 11:38:16 +0100

    association between rockpaperscissors and materials

---
+++ Assets/Scripts/Genes/RPStoMaterial_BODY.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPStoMaterial_BODY : MonoBehaviour {
+	/**
+	 * Dummy class associating RPS types to body Materials;
+	 */
+	public Material body_Neutral;
+	public Material body_Rock;
+	public Material body_Paper;
+	public Material body_Scissors;
+}
