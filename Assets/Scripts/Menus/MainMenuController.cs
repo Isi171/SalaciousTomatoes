e6a93207123aa87d0b2bad1668d343724cebfa5c
Isi171 <filippo.agalbato@cleafy.com>
2018-01-27 18:33:18 +0100

    Menus

---
+++ Assets/Scripts/Menus/MainMenuController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour 
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Score()
+    {
+        SceneManager.LoadScene("Score");
+    }
+    
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Music()
+    {
+        
+    }
+
+    public void Sound()
+    {
+        
+    }    
+}
