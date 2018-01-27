016821d9a4aba928f951647754eb37e8b6437a21
Lorenzo De Simone <lorenz.desimone@gmail.com>
2018-01-27 22:22:33 +0100

    No malus in conflict with current monster status

--- Assets/Scripts/Objectives/MalusObjective.cs
+++ Assets/Scripts/Objectives/MalusObjective.cs
@@ -8,12 +8,14 @@ public class MalusObjective : Objective {
     public int counter;
 
     public MalusObjective(int minGen, int maxGen, int b, int m, int g, Random random) {
+
         // Assign a slot to the objective.
         var slotArray = Enum.GetValues(typeof(Slot));
         slot = (Slot)slotArray.GetValue(random.Next(slotArray.Length));
         // Assign a strength to the objective.
         var geneArray = Enum.GetValues(typeof(Gene.RPS));
         gene = (Gene.RPS)geneArray.GetValue(random.Next(geneArray.Length - 1));
+
         // Assign a strength to the objective.
         generations = random.Next(minGen, maxGen);
         counter = generations;
