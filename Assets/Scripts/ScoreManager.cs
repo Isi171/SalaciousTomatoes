f255dfced9be9ca9a2709eb1a9ec3d9fd6fec7ab
Marco Ballabio <marco.ballabio@outlook.it>
2018-01-27 17:52:24 +0100

    Score menu.

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    [SerializeField] private GameObject highScore;
+    [SerializeField] private Text score;
+
+    void Start () {
+        int lastScore = PlayerPrefs.GetInt("Score");
+
+        score.text = lastScore.ToString();
+
+        if (lastScore > PlayerPrefs.GetInt("Highscore", 0)) {
+            highScore.SetActive(true);
+            PlayerPrefs.SetInt("Highscore", lastScore);
+        }
+    }
+
+    public void Retry() {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Menu() {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
