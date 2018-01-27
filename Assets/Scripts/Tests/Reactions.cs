babaa96094e182160b2955e5d87687034dcff7a6
Isi171 <filippo.agalbato@cleafy.com>
2018-01-27 10:19:45 +0100

    Input tests

---
+++ Assets/Scripts/Tests/Reactions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reactions : MonoBehaviour
+{
+    public Text text;
+    
+    public void Right()
+    {
+        text.text = "right";
+    }
+    
+    public void Left()
+    {
+        text.text = "left";
+    }
+    
+    public void Up()
+    {
+        text.text = "up";
+    }
+    
+    public void Down()
+    {
+        text.text = "down";
+    }
+}
