a4173ffd8ac9f23ef47e5bc44c72772544543b21
Federixender <federixender@hotmail.it>
2018-01-27 14:00:33 +0100

    THE COMBINATION IS REAL BBY

--- Assets/Scripts/Genes/Combine.cs
+++ Assets/Scripts/Genes/Combine.cs
@@ -3,23 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Combine : MonoBehaviour {
-	MaterialSwapper oldArms;
-	MaterialSwapper oldBody;
-	MaterialSwapper oldHead;
+	MaterialSwapper currentArms;
+	MaterialSwapper currentBody;
+	MaterialSwapper currentHead;
 	MaterialSwapper newArms;
 	MaterialSwapper newBody;
 	MaterialSwapper newHead;
 
 	void Start (){
-		oldArms = GameObject.FindGameObjectWithTag ("OldMonster").transform.Find ("Arms").gameObject.GetComponent<MaterialSwapper>();
-		oldBody = GameObject.FindGameObjectWithTag ("OldMonster").transform.Find ("Body").gameObject.GetComponent<MaterialSwapper>();
-		oldHead = GameObject.FindGameObjectWithTag ("OldMonster").transform.Find ("Head").gameObject.GetComponent<MaterialSwapper>();
+		currentArms = GameObject.FindGameObjectWithTag ("CurrentMonster").transform.Find ("Arms").gameObject.GetComponent<MaterialSwapper>();
+		currentBody = GameObject.FindGameObjectWithTag ("CurrentMonster").transform.Find ("Body").gameObject.GetComponent<MaterialSwapper>();
+		currentHead = GameObject.FindGameObjectWithTag ("CurrentMonster").transform.Find ("Head").gameObject.GetComponent<MaterialSwapper>();
 		newArms = GameObject.FindGameObjectWithTag ("NewMonster").transform.Find ("Arms").gameObject.GetComponent<MaterialSwapper>();
 		newBody = GameObject.FindGameObjectWithTag ("NewMonster").transform.Find ("Body").gameObject.GetComponent<MaterialSwapper>();
 		newHead = GameObject.FindGameObjectWithTag ("NewMonster").transform.Find ("Head").gameObject.GetComponent<MaterialSwapper>();
 	}
 
 	public void PressButton (){
-		oldArms.GeneLogic (newArms.CurrentGene);
+		currentArms.GeneLogic (newArms.CurrentGene);
+		currentBody.GeneLogic (newBody.CurrentGene);
+		currentHead.GeneLogic (newHead.CurrentGene);
 	}
 }
