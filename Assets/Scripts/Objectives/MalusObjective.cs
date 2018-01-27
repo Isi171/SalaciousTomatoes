8009a1d4df652d2d1e3078998bc3f4ad4c8c5eca
Marco Ballabio <marco.ballabio@outlook.it>
2018-01-27 11:14:15 +0100

    Done objectives.

--- Assets/Scripts/Objectives/MalusObjective.cs
+++ Assets/Scripts/Objectives/MalusObjective.cs
@@ -1,10 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
 
 public class MalusObjective : Objective {
 
     private int generations;
     private int malus;
+
+    public MalusObjective(int minGen, int maxGen, int b, int m) {
+        System.Random random = new System.Random();
+
+        // Assign a slot to the objective.
+        var slotArray = Enum.GetValues(typeof(Slot));
+        slot = (Slot)slotArray.GetValue(random.Next(slotArray.Length));
+        // Assign a strength to the objective.
+        generations = random.Next(minGen, maxGen);
+        // Set the other fields.
+        bonus = b * generations;
+        malus = m;
+    }
 
 }
