b7c4c5e5e17750466f390c29e41e6137931e1a76
Isi171 <filippo.agalbato@cleafy.com>
2018-01-28 13:52:25 +0100

    Staging for randomization at start

---
+++ Assets/Scripts/RandomizeMeAtStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeMeAtStart : MonoBehaviour
+{
+    [SerializeField] private MaterialSwapper head;
+    [SerializeField] private MaterialSwapper body;
+    [SerializeField] private MaterialSwapper limbs;
+
+    public void Start()
+    {
+        if (head != null)
+        {
+            head.Randomize();
+        }
+        if (body != null)
+        {
+            body.Randomize();
+        }
+        if (limbs != null)
+        {
+            limbs.Randomize();
+        }
+    }
+}
