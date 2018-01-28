adf99f1cfd8ab227800d0c7afd85d86853994880
Marco Ballabio <marco.ballabio@outlook.it>
2018-01-28 11:38:16 +0100

    Finished animations.

---
+++ Assets/Scripts/InputEnabler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputEnabler : MonoBehaviour {
+
+    [SerializeField] private InputHandler ih;
+
+    public void EnableInput() {
+        ih.stopInput = false;
+    }
+
+    public void DisableInput() {
+        ih.stopInput = true;
+    }
+
+}
